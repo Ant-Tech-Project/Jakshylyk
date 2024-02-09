59e19176f5ddf0b8a0ae98430aba382e18a15a0b
nurbekuulujakshylyk <nurbekuulujakshylyk@gmail.com>
2024-02-09 17:40:22 +0600

    deleted

--- day3/module.tf
+++ day3/module.tf
@@ -18,15 +18,3 @@ module "idc-provider" {
 }
 }
 
-data "aws_ami" "this" {
-  most_recent = true
-  owners      = ["amazon"]
-  filter {
-    name   = "architecture"
-    values = ["arm64"]
-  }
-  filter {
-    name   = "name"
-    values = ["al2023-ami-2023*"]
-  }
-}
