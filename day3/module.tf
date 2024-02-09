60a93ccc2512b6ccbc3df787c1fafe013071e382
nurbekuulujakshylyk <nurbekuulujakshylyk@gmail.com>
2024-02-09 17:41:10 +0600

    added ec2 terraform code

---
+++ day3/module.tf
@@ -0,0 +1,32 @@
+module "idc-provider" {
+  source = "git::https://github.com/terraform-aws-modules/terraform-aws-s3-bucket.git?ref=master"
+  bucket = "my-s3-bucket"
+  acl    = null
+
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
+  # server_side_encryption_configuration = var.server_side_encryption_configuration
+  block_public_acls   = true
+  block_public_policy = true
+  server_side_encryption_configuration = {
+  rule = {
+    bucket_key_enabled = false
+    apply_server_side_encryption_by_default = {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+}
+
+data "aws_ami" "this" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "architecture"
+    values = ["arm64"]
+  }
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023*"]
+  }
+}
